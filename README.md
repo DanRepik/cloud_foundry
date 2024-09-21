0b5401623451474cdbe5b8f1e1ad39a13d595363
Dan Repik <danrepik@gmail.com>
2024-09-21 08:15:48 -0400

    Working python function

---
+++ README.md
@@ -0,0 +1,2 @@
+# sky-forge
+Sky Forge is a curated set of cloud components that can be used to assemble cloud based applications.
---
+++ infra/__main__.py
@@ -0,0 +1,25 @@
+"""An AWS Python Pulumi program"""
+
+import cloud_foundry
+
+cloud_foundry.python_function(
+  "test-function",
+  handler="app.lambda_handler",
+  sources={
+    "app.py": """
+import json
+
+def lambda_handler(event, context):
+    name = event.get("queryStringParameters", {}).get("name", "World")
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": f"Hello, {name}!"
+        }),
+        "headers": {
+            "Content-Type": "application/json"
+        }
+    }
+"""
+  }
+)
